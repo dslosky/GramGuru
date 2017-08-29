85c504d8c862c3853abb320be3b3c0d4c2888309
Daniel Slosky <danielslosky@gmail.com>
2017-08-29 13:34:25 +0300

    Fix IUser repr

--- orm.py
+++ orm.py
@@ -68,7 +68,7 @@ class IUser(Base):
         return f.decrypt(self.password).decode()
 
     def __repr__(self):
-        return 'IUser(id={}, user={}, username={}, password={})'.format(self._user, 
+        return 'IUser(user={}, username={}, password={})'.format(self._user, 
                                                                             self.username, 
                                                                             bool(self.password))
 
